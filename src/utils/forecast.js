5b5b967ce1c45c20163c7ce8ea29ea99b12f27c6
gustavoarc <gustavoarc@gmail.com>
2021-09-16 17:16:29 -0500

    Add higt and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+            console.log (response.body)
+            callback(undefined, response.body.current.weather_descriptions[0] + 
+                                " The humidity is " + response.body.current.humidity + " % "+
+                                ". It is currently " + response.body.current.temperature + 
+                                " degress out.")
         }
     })
 }
